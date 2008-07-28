0c632607bf39b1aaf9a97bae86e98ead5e2002c2
jensens <jensens@12379f12-61e2-0310-a953-c5ab0a7db1c5>
2008-07-28 23:44:26 +0000

    remove AllowedTypesByIface. these days its so easy to extend the FTI within an own profile.

--- Products/ATVocabularyManager/tools/vocabularylib.py
+++ Products/ATVocabularyManager/tools/vocabularylib.py
@@ -18,12 +18,9 @@ from OFS.Cache import Cacheable
 from Products.PlacelessTranslationService.Negotiator import getLangPrefs
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.utils import UniqueObject
-from Products.CMFCore.TypesTool import FactoryTypeInformation
-from Products.CMFCore.permissions import AddPortalContent
 from Products.CMFCore.permissions import ModifyPortalContent
 from Products.CMFCore.permissions import View
 from Products.Archetypes.atapi import *
-from Products.Archetypes.AllowedTypesByIface import AllowedTypesByIfaceMixin
 from Products.Archetypes.interfaces.vocabulary import IVocabulary
 from Products.ATVocabularyManager.config import TOOL_NAME
 from Products.ATVocabularyManager.config import DEFAULT_VOCABULARY_CONTAINER
@@ -34,7 +31,7 @@ from Products.Archetypes.utils import shasattr
 _vocabterm_types = {}
 
 def registerVocabularyTerm(klass, container_name=DEFAULT_VOCABULARY_CONTAINER):
-    """ register a IVocabularyTerm implementing class and map it to a container """
+    """register a IVocabularyTerm implementing class and map it to container """
     if not container_name in _vocabterm_types.keys():
         _vocabterm_types[container_name] = []
     _vocabterm_types[container_name].append(klass)
@@ -44,36 +41,16 @@ def registerVocabularyContainer(klass):
     pass
 
 ### note: derive somewhere in future from BaseTool
-class VocabularyLibrary(UniqueObject, AllowedTypesByIfaceMixin, OrderedBaseFolder, Cacheable):
+class VocabularyLibrary(UniqueObject, OrderedBaseFolder, Cacheable):
     '''
     This tool contains dynamic vocabularies to be used by Archetypes
     fields. It has methods to register special/custom vocabulary
     types
     '''
     implements(IATVocabularyLibrary)
-    
-    __implements__ = (getattr(UniqueObject,'__implements__',()),) + \
-                     (getattr(AllowedTypesByIfaceMixin,'__implements__',()),) + \
-                     (getattr(OrderedBaseFolder,'__implements__',()),)
 
     security = ClassSecurityInfo()
-    portal_type = meta_type = 'VocabularyLibrary'
-    archetype_name          = 'Vocabulary Library'
-    allowed_content_types   = ()
-    allow_discussion        = False
-    immediate_view          = 'vocabularytool_view'
-    default_view            = 'vocabularytool_view'
-    global_allow            = False
-    filter_content_types    = False
-    allowed_interfaces      = (IVocabulary,)
-    actions                 =  (
-           {'action':      'string:$object_url/vocabularytool_view',
-            'category':    'object',
-            'id':          'view',
-            'name':        'View',
-            'permissions': ('View',),
-            'condition'  : 'python:True'},
-     )
+    meta_type = 'VocabularyLibrary'
 
     id_field = OrderedBaseFolderSchema['id'].copy()
     id_field.mode = 'r'
@@ -81,19 +58,6 @@ class VocabularyLibrary(UniqueObject, AllowedTypesByIfaceMixin, OrderedBaseFolde
     title_field.mode = 'r'
 
     manage_options = OrderedBaseFolder.manage_options + Cacheable.manage_options
-
-
-    def _verifyObjectPaste(self, object, validate_src=1):
-        # XXX: Archetyps' AllowedTypesByIface is broken, this override is
-        # necessary to avoid an error
-        pt = getToolByName(self, 'portal_types')
-        ti = pt.getTypeInfo(self.portal_type)
-        pt.manage_delObjects([self.portal_type])
-        # Changed this from using PortalFolder to using OrderedBaseFolder,
-        # without this change, we'll get an error
-        value = OrderedBaseFolder._verifyObjectPaste(self, object, validate_src)
-        pt._setObject(self.portal_type, ti)
-        return value
 
     schema=OrderedBaseFolderSchema  + Schema((
         # a tool doesnt need an idwidget and title for edit
