a504d9e157cd89940cf9703bfc981f755c79ccf8
WouterVH <WouterVH@12379f12-61e2-0310-a953-c5ab0a7db1c5>
2011-04-05 18:34:07 +0000

    change version from 1.7dev to 1.6.1dev

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.7dev'
+version = '1.6.1dev'
 
 long_description = (
     read('README.txt')
