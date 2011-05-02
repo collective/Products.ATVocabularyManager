ad3dfcdc9056362d15f70bbb9f9e78f720dde3fe
jfroche <jfroche@12379f12-61e2-0310-a953-c5ab0a7db1c5>
2011-05-02 12:50:05 +0000

    Back to development: 1.6.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.6.1'
+version = '1.6.2dev'
 
 long_description = (
     read('README.txt')
