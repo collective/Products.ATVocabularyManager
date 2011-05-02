23fe6517bd709ad83fc81a3d880832df9776ff4c
jfroche <jfroche@12379f12-61e2-0310-a953-c5ab0a7db1c5>
2011-05-02 12:48:47 +0000

    Preparing release 1.6.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.6.1dev'
+version = '1.6.1'
 
 long_description = (
     read('README.txt')
